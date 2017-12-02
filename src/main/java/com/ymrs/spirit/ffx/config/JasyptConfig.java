3a781ba12e444b5071e4d3ee882ff9574c909fb8
dante <sunchao.0129@163.com>
2017-12-02 21:48:58 +0800

    添加属性文件加密处理类

---
+++ src/main/java/com/ymrs/spirit/ffx/config/JasyptConfig.java
@@ -0,0 +1,27 @@
+package com.ymrs.spirit.ffx.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+	@Bean(name = "spiritEncryptorBean")
+	public StringEncryptor stringEncryptor() {
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword("Spirit@FFX!1985");
+		config.setAlgorithm("PBEWithMD5AndDES");
+		config.setKeyObtentionIterations("1000");
+		config.setPoolSize("1");
+		config.setProviderName("SunJCE");
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setStringOutputType("RSA256");
+		encryptor.setConfig(config);
+		return encryptor;
+	}
+	
+}
--- src/test/java/com/ymrs/spirit/ffx/SpiritApplicationTests.java
+++ src/test/java/com/ymrs/spirit/ffx/SpiritApplicationTests.java
@@ -1,11 +1,22 @@
 package com.ymrs.spirit.ffx;
 
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpiritApplicationTests {
+	@Autowired
+	@Qualifier("spiritEncryptorBean")
+    private StringEncryptor stringEncryptor;
 	
+	@Test
+	public void xx() {
+		System.out.println(stringEncryptor.encrypt("iamdante"));
+	}
 }
